0ab586964928a5718ea408f4342bcdd64775a2eb
pandas9 <81386636+pandas9@users.noreply.github.com>
2025-01-22 16:32:27 +0800

    v

--- components/line-shadow-text.tsx
+++ components/line-shadow-text.tsx
@@ -26,7 +26,7 @@ export function LineShadowText({
     <MotionComponent
       style={{ "--shadow-color": shadowColor } as React.CSSProperties}
       className={cn(
-        "relative z-0 inline-flex ml-9",
+        "relative z-0 inline-flex ml-3 md:ml-9",
         "after:absolute after:left-[0.04em] after:top-[0.04em] after:content-[attr(data-text)]",
         "after:bg-[linear-gradient(45deg,transparent_45%,var(--shadow-color)_45%,var(--shadow-color)_55%,transparent_0)]",
         "after:-z-10 after:bg-[length:0.06em_0.06em] after:bg-clip-text after:text-transparent",
